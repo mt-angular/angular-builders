0d1f36147f82ed46fff8f06925be4a2e9003c595
Thomas Milotti <thomas.milotti@gmail.com>
2019-08-22 12:40:35 +0200

    custom-webpack/dev-server setup improvement

--- packages/custom-webpack/src/dev-server/index.ts
+++ packages/custom-webpack/src/dev-server/index.ts
@@ -10,16 +10,34 @@ import { from, Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { getTransforms } from '../common';
 import { CustomWebpackDevServerSchema } from '../custom-webpack-schema';
+import { json } from '@angular-devkit/core';
 
 
 export const serveCustomWebpackBrowser = (options: DevServerBuilderOptions, context: BuilderContext): Observable<DevServerBuilderOutput> => {
     async function setup() {
         const browserTarget = targetFromTargetString(options.browserTarget);
-        return context.getTargetOptions(browserTarget) as unknown as CustomWebpackDevServerSchema;
+        // return context.getTargetOptions(browserTarget) as unknown as CustomWebpackDevServerSchema;
+
+        // INSPIRED FROM angular-cli/packages/angular_devkit/build_angular/src/dev-server/index.ts
+        // Get the browser configuration from the target name.
+        const rawBrowserOptions = await context.getTargetOptions(browserTarget) as CustomWebpackDevServerSchema;
+        const opt = { ...options };
+
+        // In dev server we should not have budgets because of extra libs such as socks-js
+        opt.budgets = undefined;
+
+        const browserName = await context.getBuilderNameForTarget(browserTarget);
+        const browserOptions = await context.validateOptions<json.JsonObject & CustomWebpackDevServerSchema>(
+            { ...rawBrowserOptions, ...opt },
+            browserName,
+        );
+
+        return browserOptions;
+
     }
 
     return from(setup())
         .pipe(switchMap(customWebpackOptions => executeDevServerBuilder(options, context, getTransforms(customWebpackOptions, context))));
-}
+};
 
 export default createBuilder<DevServerBuilderOptions, DevServerBuilderOutput>(serveCustomWebpackBrowser);
